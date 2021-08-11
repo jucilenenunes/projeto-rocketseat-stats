9d67f1af37f0e7e283431fcce3e2330aca93519c
Jucilene Nunes <jucilenenunes10@gmail.com>
2021-08-11 19:20:57 -0300

    integrando modulos

--- stats.js
+++ stats.js
@@ -1,21 +1,32 @@
 const os = require('os')
+const log = require('./logger')
+
+Date.prototype.addHours= function(h){
+    this.setHours(this.getHours()+h);
+    return this;
+}
 
 setInterval(() => {
 
-const { freemem, totalmem} = os
+    const { freemem, totalmem} = os
 
-const total = parseInt(totalmem() / 1024 / 1024)
-const mem = parseInt(freemem() /10224 / 1024)
-const percents = parseInt(mem/total * 100)
+    const total = parseInt(totalmem() / 1024 / 1024)
+    const mem = parseInt(freemem() /10224 / 1024)
+    const percents = parseInt(mem/total * 100)
 
-const stats = {
-    free: `${mem} MB`,
-    total:`${total} MB`,
-    usage: `${percents} %`
-}
+    const stats = {
+        date: new Date().addHours(-3),
+        free: `${mem} MB`,
+        total:`${total} MB`,
+        usage: `${percents} %`
+    }
 
-console.clear()
-console.log("===PC STATS===")
-console.table(stats)
+    console.clear()
+    console.log("===PC STATS===")
+    console.table(stats)
 
+
+    log(`${JSON.stringify(stats)}\n`)
+
+    
 }, 1000)
